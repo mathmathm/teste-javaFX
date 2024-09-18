a5d279a2b905c61e86c10c72f680fcb451dc3bb8
ForestTi <36780057+mathmathm@users.noreply.github.com>
2024-09-18 11:44:28 -0300

    Delete api-cadastro-usuario/api-cadastro-usuario directory

--- api-cadastro-usuario/api-cadastro-usuario/src/main/java/com/api/cadastro/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.cadastro.repository;
-
-
-import com.api.cadastro.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
